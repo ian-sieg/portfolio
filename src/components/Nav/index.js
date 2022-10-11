58b18adef5d60360959209c731c4b7dd93a113c7
ian.moss <ianmsieg@gmail.com>
2022-10-11 13:47:28 -0500

    added basic components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,10 @@
+import headshot from "../../images/headshot.png"
+
+export default function Nav() {
+    return(
+        <div>
+            <h1>Ian Sieg</h1>
+            <img src={headshot} alt="Ian Sieg wearing a light blue button up shirt"/>
+        </div>
+    )
+}

2aef1806d1ddc18d624b24fbeffbbe4817556f57
ian.moss <ianmsieg@gmail.com>
2022-10-31 21:10:41 -0500

    mostly finished the portfolio section functionality and basic styling

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,22 +7,32 @@ import './style.css'
 
 export default function Projects (props) {
     const scrollRef = useHorizontalScroll()
-    //TODO: finish this so the overlay doesn't show up unless on mouseover
-    const mouseEvent = (e) => {
-        console.log(e)
+
+    const mouseEnter = (e) => {
+        if (e.target.nextSibling.classList.contains('card-content')) {
+            e.target.nextSibling.classList.toggle('invis')
+        }
     }
+    const mouseExit = (e) => {
+        if (e.target.classList.contains('card-content')){
+            e.target.classList.toggle('invis')
+        } else if (e.target.offsetParent.classList.contains('card-content')){
+            e.target.offsetParent.classList.toggle('invis')
+        }
+    }
+
     return(
             <div ref={scrollRef} className='container-scroll'>
                 <div className="row-scroll">
                 {props.projArr.map(proj => 
-                    <Card className='card-sizing' onMouseEnter={mouseEvent} key={proj.id}>
-                        <Card.Img src={proj.image} className='img-sizing'/>
-                        <Card.ImgOverlay className="card-content">
+                    <Card className='card-sizing' key={proj.id}>
+                        <Card.Img src={proj.image} className='img-sizing' onMouseEnter={mouseEnter}/>
+                        <Card.ImgOverlay className="card-content invis"  onMouseLeave={mouseExit} >
                             <Card.Body>
-                                <Card.Title>{proj.title}</Card.Title>
+                                <Card.Title className='mb-3'>{proj.title}</Card.Title>
                                 <Card.Text>{proj.info}</Card.Text>
-                                <Button href={proj.repo} target='_blank'><i className="fa-brands fa-github"/></Button>
-                                <Button href={proj.link} target='_blank'><i className="fa-solid fa-link"/></Button>
+                                <Button href={proj.repo} target='_blank' rel='noreferrer' className="card-btns"><i className="fa-brands fa-github"/></Button>
+                                <Button href={proj.link} target='_blank' rel='noreferrer' className="card-btns"><i className="fa-solid fa-link"/></Button>
                             </Card.Body>
                         </Card.ImgOverlay>
                     </Card>

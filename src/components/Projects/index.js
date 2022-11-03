ac906ead834526d936bd40afebca47e116ecb1ab
ian.moss <ianmsieg@gmail.com>
2022-11-03 12:05:27 -0500

    fixed some sizing issues on different breakpoints;

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -51,13 +51,28 @@ export default function Projects (props) {
                         <Card.Img src={proj.image} className='img-sizing' onMouseEnter={mouseEnter}/>
                         <Card.ImgOverlay className="card-content invis"  onMouseLeave={mouseExit} >
                             <Card.Body>
-                                <Card.Title className='mb-3'><b>{proj.title}</b></Card.Title>
+                                <Card.Title className='mb-3'><b>{proj.title}</b>
+                                {width <= 1300 ? 
+                                    <>
+                                    <Button href={proj.repo} target='_blank' rel='noreferrer' variant='dark' className="card-btns first-btn"><i className="fa-brands fa-github"/></Button>
+                                    <Button href={proj.link} target='_blank' rel='noreferrer' variant='dark' className="card-btns"><i className="fa-solid fa-link"/></Button>
+                                    </>
+                                    :
+                                    null}
+                                </Card.Title>
                                 <hr/>
                                 <Card.Text><b>Description:</b> {proj.info}</Card.Text>
                                 <Card.Text><b>Tools:</b> {proj.tools}</Card.Text>
                                 {proj.resp ? <Card.Text><b>Core Responsibilities:</b> {proj.resp}</Card.Text> : null}
-                                <Button href={proj.repo} target='_blank' rel='noreferrer' className="card-btns" variant="dark"><i className="fa-brands fa-github"/></Button>
-                                <Button href={proj.link} target='_blank' rel='noreferrer' className="card-btns" variant="dark"><i className="fa-solid fa-link"/></Button>
+
+                                {width > 1300 ?
+                                    <>
+                                    <Button href={proj.repo} target='_blank' rel='noreferrer' variant='dark' className="card-btns"><i className="fa-brands fa-github"/></Button>
+                                    <Button href={proj.link} target='_blank' rel='noreferrer' variant='dark' className="card-btns"><i className="fa-solid fa-link"/></Button>
+                                    </>
+                                    :
+                                    null
+                                }
                             </Card.Body>
                         </Card.ImgOverlay>
                     </Card>
